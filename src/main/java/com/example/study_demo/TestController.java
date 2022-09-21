52b3bf63d58cbf9f9c58e521161213a554e0584c
jiabing <jiabing@xdf.cn>
2022-09-21 16:01:34 +0800

    push

--- src/main/java/com/example/study_demo/TestController.java
+++ src/main/java/com/example/study_demo/TestController.java
@@ -1,5 +1,12 @@
 package com.example.study_demo;
 
 
+import org.joda.time.DateTime;
+
 public class TestController {
+
+
+    public static void main(String[] args) {
+        DateTime dateTime = new DateTime().withTime(15, 0, 0, 0);
+    }
 }
